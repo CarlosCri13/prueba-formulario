fb9cc06ec1d100c6ca0fc7fc80612db845cd2412
CarlosCri13 <110143107+CarlosCri13@users.noreply.github.com>
2024-09-03 20:05:25 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('sugerenciasForm').addEventListener('submit', function(event) {
+    // Aquí puedes agregar validaciones adicionales o mostrar un mensaje de confirmación antes de enviar.
+    alert("Tu sugerencia se está enviando...");
+});

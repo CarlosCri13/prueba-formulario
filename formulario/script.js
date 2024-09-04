a0263965e1f05faad283061454d2ebb31e7f3272
CarlosCri13 <110143107+CarlosCri13@users.noreply.github.com>
2024-09-03 20:03:14 -0500

    Delete formulario directory

--- formulario/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('sugerenciasForm').addEventListener('submit', function(event) {
-    // Aquí puedes agregar validaciones adicionales o mostrar un mensaje de confirmación antes de enviar.
-    alert("Tu sugerencia se está enviando...");
-});
